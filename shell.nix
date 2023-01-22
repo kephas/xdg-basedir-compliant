0ec8c1d52ae471a8140010a464bffa5bfd603072
Pierre Thierry <pierre@nothos.net>
2023-01-22 18:41:43 +0100

    Add build tools in Nix shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  packages = [ (pkgs.haskellPackages.ghcWithPackages (p: [p.aeson p.bytestring p.containers p.directory p.hspec p.path p.polysemy p.polysemy-plugin p.polysemy-zoo p.split])) ];
+  packages = [ pkgs.hpack pkgs.cabal-install (pkgs.haskellPackages.ghcWithPackages (p: [p.aeson p.bytestring p.containers p.directory p.hspec p.path p.polysemy p.polysemy-plugin p.polysemy-zoo p.split])) ];
 }
